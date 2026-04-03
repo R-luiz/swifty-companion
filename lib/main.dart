538b66f351655b56d1aff05be94f3c37a9154014
R-Luiz <lerlfr3@gmail.com>
2026-04-03 16:18:00 +0200

    startup crash fix attempt

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'search_page.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   runApp(const SwiftyApp());
 }
